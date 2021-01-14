a9a9abb88a0c4320f250c05e8527cc8701b0aac7
devdepijama <hugo@devdepijama.com.br>
2021-01-14 23:20:24 +0000

    Added compiled OpenSSL x64 (static)

--- btc-freedom/Sha256.cu
+++ btc-freedom/Sha256.cu
@@ -1,5 +1,17 @@
 #include "Sha256.cuh"
 
-int Sha256::hash(uint8_t* data, size_t len, uint8_t* output) {
-	return 0;
+Sha256::Sha256() {
+
+}
+
+int Sha256::hash(uint8_t* data, size_t len, uint8_t output[SHA256_DIGEST_LENGTH]) {
+    SHA256_Init(&(this->sha256));
+    SHA256_Update(&(this->sha256), data, len);
+    SHA256_Final(output, &sha256);
+
+    return 0;
+}
+
+Sha256::~Sha256() {
+
 }
--- btc-freedom/Sha256.cuh
+++ btc-freedom/Sha256.cuh
@@ -2,11 +2,17 @@
 #define H_SHA256
 
 #include <stdint.h>
+#include <openssl/sha.h>
 
 class Sha256
 {
+	private:
+		SHA256_CTX sha256;
+
 	public:
-		static int hash(uint8_t* data, size_t len, uint8_t* output);
+		Sha256();
+		int hash(uint8_t* data, size_t len, uint8_t output[SHA256_DIGEST_LENGTH]);
+		~Sha256();
 };
 
 #endif
