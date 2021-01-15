3e50e3a600e024a2216fca3fd9739406c27c0b89
devdepijama <hugo@devdepijama.com.br>
2021-01-15 00:47:43 +0000

    Added secp256k1 dependency. Unfortunately.. it encapsulates alloc

--- btc-freedom/EllipticalCurve.cu
+++ btc-freedom/EllipticalCurve.cu
@@ -1,5 +1,15 @@
 #include "EllipticalCurve.cuh"
 
+#include <stdlib.h>
+
+EllipticalCurve::EllipticalCurve() {
+	this->ctx = secp256k1_context_create(SECP256K1_CONTEXT_NONE);
+}
+
 int EllipticalCurve::calculatePublicKey(uint8_t* data, size_t len, uint8_t* output) {
+	secp256k1_ec_pubkey_create(this->ctx, (secp256k1_pubkey *) output, data);
 	return 0;
+}
+
+EllipticalCurve::~EllipticalCurve() {
 }
